d9dcca6809ddf698df04d6cbfd27581c3b2fd254
João Pedro de França Barboza <franca.joao@academico.ifrn.edu.br>
2023-01-18 14:06:23 -0300

    Aula 03 form1

---
+++ grails-app/domain/com/hmtmcse/ocb/Member.groovy
@@ -0,0 +1,36 @@
+package com.hmtmcse.ocb
+
+class Member {
+
+    Integer id
+    String firstName
+    String lastName
+    String email
+    String password
+    String memberType = GlobalConfig.USER_TYPE.REGULAR_MEMBER
+    String identityHash
+    Date identityHashLastUpdate
+    Boolean isActive = true
+
+    Date dataCreated
+    Date lastUpdated
+
+    //adc constraints
+    static constraints = {
+        email(email: true, nullable: false, unique: true, blank: false)
+        password(blank: false)
+        lastName(nullable: true)
+        identityHash(nullable: true)
+        identityHashLastUpdate(nullable: true)
+    }
+
+
+    // adc events
+    def beforeInsert() {
+        this.password = this.password.encodeAsMD5()
+    }
+
+    def beforeUpdate() {
+        this.password = this.password.encodeAsMD5()
+    }
+}
